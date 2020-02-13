00bfc5c041bf3b9ebd18b8dcbc85b87e942db6d4
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-02-13 00:25:33 -0800

    save

--- with_additional_args/test.cpp
+++ with_additional_args/test.cpp
@@ -2,19 +2,16 @@
 
 // `drop_first_arg` takes a function type as template argument
 // and returns the function type with first argument dropped
-// The implementation is copied from ATen/detail/FunctionTraits.h
+// The implementation is modified from ATen/detail/FunctionTraits.h
 
 template <typename T>
-struct drop_first_arg_helper : public drop_first_arg_helper<decltype(&T::operator())> {
-};
+struct drop_first_arg_helper : public drop_first_arg_helper<decltype(&T::operator())> {};
 
 template <typename ClassType, typename T>
-struct drop_first_arg_helper<T ClassType::*> : public drop_first_arg_helper<T> {
-};
+struct drop_first_arg_helper<T ClassType::*> : public drop_first_arg_helper<T> {};
 
 template <typename ClassType, typename ReturnType, typename... Args>
-struct drop_first_arg_helper<ReturnType(ClassType::*)(Args...) const> : public drop_first_arg_helper<ReturnType(Args...)> {
-};
+struct drop_first_arg_helper<ReturnType(ClassType::*)(Args...) const> : public drop_first_arg_helper<ReturnType(Args...)> {};
 
 template<typename return_type, typename first, typename... arg_types>
 struct drop_first_arg_helper<return_type (first, arg_types...)> {
