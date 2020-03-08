ee870dfcb24f72264a5ae9fbc11cc5676b673663
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-03-08 16:38:02 -0700

    save

--- test3.py
+++ test3.py
@@ -1,5 +1,8 @@
 import torch
 import torchvision.models as models
+torch.backends.cudnn.benchmark=False
+torch.backends.cudnn.deterministic=True
+
 resnet = models.resnet18().half().cuda()
 t = torch.ones([1, 3, 10210, 8641], dtype=torch.float16, device="cuda")
 output = resnet(t)
