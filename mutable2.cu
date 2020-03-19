bffc9202ab0a4b2ae5771d2714e4f921074b966e
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-03-18 18:22:17 -0700

    save

--- mutable2.cu
+++ mutable2.cu
@@ -1,31 +1,32 @@
 template<typename func_t>
-__host__ __device__ void invoke_impl(func_t &f) {
-    f();
+__global__ void elementwise_kernel(func_t f) {
+  f();
 }
 
 template<typename func_t>
-__host__ __device__ void invoke(func_t &f) {
-    invoke_impl(f);
+void launch_kernel(func_t f) {
+  elementwise_kernel<func_t><<<1, 1>>>(f);
 }
 
 template<typename func_t>
-__global__ void kernel(func_t g) {
-    g();
+__host__ __device__ float invoke_impl(func_t &f) {
+  return f();
 }
 
 template<typename func_t>
-void launch_kernel(func_t g) {
-    kernel<<<1, 1>>>(g);
+__host__ __device__ float invoke(func_t &f) {
+  return invoke_impl(f);
 }
 
 template<typename func_t>
 void gpu_kernel_impl(func_t f) {
-    launch_kernel([=]__host__ __device__() mutable {
-        invoke<func_t>(f);
-    });
+  launch_kernel([=]__host__ __device__() mutable {
+    float *out = nullptr;
+    *out = invoke<func_t>(f);
+  });
 }
 
 int main() {
-    int a = 0;
-    gpu_kernel_impl([=]__host__ __device__() mutable { return a++; });
+  int a = 0;
+  gpu_kernel_impl([=]__host__ __device__() mutable { return a++; });
 }
