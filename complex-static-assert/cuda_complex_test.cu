c867fb8cea42487615def5ce6741ab7cf05b8598
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-08-12 17:03:20 -0700

    --

--- complex-static-assert/cuda_complex_test.cu
+++ complex-static-assert/cuda_complex_test.cu
@@ -1,16 +1,51 @@
-#include "complex.h"
+#define C10_HOST_DEVICE __host__ __device__
+
+template<typename T>
+struct alignas(sizeof(T) * 2) complex {
+  using value_type = T;
+
+  T real_ = T(0);
+  T imag_ = T(0);
+
+  constexpr complex() = default;
+  constexpr complex(const T& re, const T& im = T()): real_(re), imag_(im) {}
+
+  template<typename U>
+  constexpr complex<T> &operator =(const complex<U> &rhs) {
+    real_ = rhs.real();
+    imag_ = rhs.imag();
+    return *this;
+  }
+
+  template<typename U>
+  constexpr complex<T> &operator -=(const complex<U> &rhs) {
+    real_ -= rhs.real();
+    imag_ -= rhs.imag();
+    return *this;
+  }
+
+  constexpr T real() const {
+    return real_;
+  }
+  constexpr T imag() const {
+    return imag_;
+  }
+};
+
+constexpr complex<double> operator"" _id(long double imag) {
+  return complex<double>(0.0, static_cast<double>(imag));
+}
 
 template<typename scalar_t, typename rhs_t>
-constexpr c10::complex<scalar_t> m(scalar_t real, scalar_t imag, c10::complex<rhs_t> rhs) {
-  c10::complex<scalar_t> result(real, imag);
+constexpr complex<scalar_t> m(scalar_t real, scalar_t imag, complex<rhs_t> rhs) {
+  complex<scalar_t> result(real, imag);
   result -= rhs;
   return result;
 }
 
 template<typename scalar_t>
 C10_HOST_DEVICE void test_arithmetic_assign_complex() {
-  using namespace c10::complex_literals;
-  constexpr c10::complex<scalar_t> y3 = m(scalar_t(2), scalar_t(2), 1.0_id);
+  constexpr complex<scalar_t> y3 = m(scalar_t(2), scalar_t(2), 1.0_id);
   static_assert(y3.real() == scalar_t(2), "");
 #if !defined(__CUDACC__)
   // The following is flaky on nvcc
