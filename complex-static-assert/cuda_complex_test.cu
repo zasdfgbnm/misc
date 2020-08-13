7fb849270f0a251bd935bff7ed403cde96d62970
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-08-12 17:13:00 -0700

    --

--- complex-static-assert/cuda_complex_test.cu
+++ complex-static-assert/cuda_complex_test.cu
@@ -1,29 +1,24 @@
 template<typename T>
-struct alignas(sizeof(T) * 2) complex {
-  using value_type = T;
-
+struct complex {
   T real_ = T(0);
-  T imag_ = T(0);
 
-  constexpr complex(const T& re, const T& im = T()): real_(re), imag_(im) {}
+  constexpr complex(const T& re): real_(re) {}
 
-  template<typename U>
-  constexpr complex<T> &operator -=(const complex<U> &rhs) {
-    real_ -= rhs.real_;
-    imag_ -= rhs.imag_;
+  constexpr complex<T> &operator -=(double rhs) {
+    real_ -= rhs;
     return *this;
   }
 };
 
-template<typename scalar_t, typename rhs_t>
-constexpr complex<scalar_t> m(scalar_t real, scalar_t imag, complex<rhs_t> rhs) {
-  complex<scalar_t> result(real, imag);
+template<typename scalar_t>
+constexpr complex<scalar_t> m(scalar_t real, double rhs) {
+  complex<scalar_t> result(real);
   result -= rhs;
   return result;
 }
 
 __global__ void test_arithmetic_assign() {
-  constexpr complex<float> y3 = m(float(2), float(2), complex<double>(0.0, 1.0));
+  constexpr complex<float> y3 = m(float(2), 0.0);
   static_assert(y3.real_ == float(2), "");
 }
 
