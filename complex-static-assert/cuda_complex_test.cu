8a4ae75cdaf95da1a028e02c19cb08b7e2568902
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-08-12 17:14:13 -0700

    --

--- complex-static-assert/cuda_complex_test.cu
+++ complex-static-assert/cuda_complex_test.cu
@@ -1,24 +1,22 @@
-template<typename T>
 struct complex {
-  T real_ = T(0);
+  float real_ = 0;
 
-  constexpr complex(const T& re): real_(re) {}
+  constexpr complex(const float& re): real_(re) {}
 
-  constexpr complex<T> &operator -=(double rhs) {
+  constexpr complex &operator -=(double rhs) {
     real_ -= rhs;
     return *this;
   }
 };
 
-template<typename scalar_t>
-constexpr complex<scalar_t> m(scalar_t real, double rhs) {
-  complex<scalar_t> result(real);
+constexpr complex m(float real, double rhs) {
+  complex result(real);
   result -= rhs;
   return result;
 }
 
 __global__ void test_arithmetic_assign() {
-  constexpr complex<float> y3 = m(float(2), 0.0);
+  constexpr complex y3 = m(float(2), 0.0);
   static_assert(y3.real_ == float(2), "");
 }
 
