601aa535f29d6e5fe85c591f034d3b205a23971e
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-09-09 12:05:46 -0700

    update

--- cusolver.cu
+++ cusolver.cu
@@ -1,9 +1,12 @@
+#include <cuda_runtime.h>
+#include <cusolverDn.h>
+
 int main() {
     void *p[10];
     cusolverDnHandle_t handle;
     cudaMalloc(&p[0], 100000);
     cudaMalloc(&p[1], 100000);
-    cusolverDnCreate(handle);
+    cusolverDnCreate(&handle);
     cudaMalloc(&p[2], 100000);
     cudaMalloc(&p[3], 100000);
 }
