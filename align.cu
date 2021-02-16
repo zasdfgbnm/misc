991eefd26962707958a3c6dc9f59177a6fc621c2
Gao, Xiang <qasdfgtyuiop@gmail.com>
2021-02-16 10:51:29 -0800

    Create align.cu

---
+++ align.cu
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <tuple>
+
+struct alignas(16) A {
+    double data[2];
+};
+
+__global__ void align() {
+    using tup = std::tuple<bool, A>;
+    printf("%d", (int)alignof(tup));
+}
+
+int main() {
+    using tup = std::tuple<bool, A>;
+    std::cout << alignof(tup) << std::endl;
+    align <<<1, 1>>> ();
+}
