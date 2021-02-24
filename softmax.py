c85ad07470f45a70eaa0055bc3f0ff34d9f2f79f
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-02-24 09:42:09 -0800

    save

--- softmax.py
+++ softmax.py
@@ -1,6 +1,5 @@
 import torch
-x = torch.randn((64, 400, 120, 1024), device="cuda:0", dtype=torch.float16)
-torch.cuda.profiler.start()
-y = torch.nn.functional.log_softmax(x, dim=-1)
-torch.cuda.profiler.stop()
+x = torch.randn((1100000000, 2), device="cuda:0", dtype=torch.float16, requires_grad=True)
+y = torch.nn.functional.log_softmax(x, dim=-1, dtype=torch.float32)
+y.backward(y)
 torch.cuda.synchronize()
