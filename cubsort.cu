3cefe074f31c0cfd39af13ac821996c5f9ffe275
Xiang Gao <qasdfgtyuiop@gmail.com>
2021-03-11 18:44:59 -0800

    save

--- cubsort.cu
+++ cubsort.cu
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cub/cub.cuh>
+#include <chrono>
+
+using namespace std::chrono;
 
 constexpr size_t N = 1000000;
 
@@ -19,10 +22,17 @@ int main() {
     size_t temp_storage_bytes;
     cub::DeviceRadixSort::SortPairs(nullptr, temp_storage_bytes, d1, d2, d3, d4, N);
     cudaMalloc(&tmp, temp_storage_bytes);
+
+    cudaDeviceSynchronize();
+    auto start = high_resolution_clock::now();
     cub::DeviceRadixSort::SortPairs(tmp, temp_storage_bytes, d1, d2, d3, d4, N);
+    cudaDeviceSynchronize();
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    std::cout << duration.count() << std::endl;
 
     cudaMemcpy(h2, d2, sizeof(int) * N, cudaMemcpyDefault);
-    for(size_t i = 0; i < N; i++) {
+    for(size_t i = 0; i < 10; i++) {
         std::cout << h2[i] << ", ";
     }
 }
